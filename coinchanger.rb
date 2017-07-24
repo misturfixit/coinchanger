59705209c0ad824d978ad0735e8c057e397d18c3
misturfixit <japullins420@gmail.com>
2017-07-24 07:41:40 -0400

    mmhmm

--- coinchanger.rb
+++ coinchanger.rb
@@ -4,11 +4,21 @@
 
 def coiny(change)
 	coins = {quarter: 25, dime: 10, nikkel: 5, penny: 1}
-	if change == 1 
-		{quarter: 0, dime: 0, nikkel: 0, penny: 1}
-	elsif change == 5
-		{quarter: 0, dime: 0, nikkel: 1, penny: 0}
-	end
+	
+		if change == 1 
+			{quarter: 0, dime: 0, nikkel: 0, penny: 1}
 		
+		elsif change == 5
+			{quarter: 0, dime: 0, nikkel: 1, penny: 0}
+
+		elsif change == 10
+			{quarter: 0, dime: 1, nikkel: 0, penny: 0}
+
+		elsif change == 25
+			{quarter: 1, dime: 0, nikkel: 0, penny: 0}
+			
+
+		end					
+			
 				
 end
--- cointest.rb
+++ cointest.rb
@@ -7,23 +7,23 @@ class TestArr < Minitest::Test
 		crackers = 1
 		crakkers = 3
 		assert_equal(4, (crackers + crakkers))
-
 	end	
 
 	def test_for_apenny
 		assert_equal({quarter: 0, dime: 0, nikkel: 0, penny: 1}, coiny(1))
 	end
 	
-
 	def test_a_nickel
-		
 		assert_equal({quarter: 0, dime: 0, nikkel: 1, penny: 0}, coiny(5))
 	end		
 
+	def test_a_dime
+		assert_equal({quarter: 0, dime: 1, nikkel: 0, penny: 0}, coiny(10))
+	end	
 
-
-
-
+	def test_a_qtr
+		assert_equal({quarter: 1, dime: 0, nikkel: 0, penny: 0}, coiny(25))
+	end	
 
 
 end	
